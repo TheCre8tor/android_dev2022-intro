948250a966cded843bb2e2adf20e6b9751ea08b4
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-09-11 22:01:59 +0100

    Completed the first day lecture

--- app/src/main/java/com/example/android_dev2022intro/SecondActivity.kt
+++ app/src/main/java/com/example/android_dev2022intro/SecondActivity.kt
@@ -1,12 +1,18 @@
 package com.example.android_dev2022intro
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
 
+        val username = intent.getStringExtra("USER")
+        val textView = findViewById<TextView>(R.id.tvOffer)
 
+        val message = "$username, you will get free access to all the content for one month"
+        textView.text = message
     }
 }
